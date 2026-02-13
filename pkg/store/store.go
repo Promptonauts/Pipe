24c9212be48f0e9411d48e37818f66eb12018856
GUNAVARDHANAREDDY TAMMA <2300032314@kluniversity.in>
2026-02-13 05:19:02 +0000

    added store

---
+++ pkg/store/store.go
@@ -0,0 +1,40 @@
+package store
+
+import (
+	"github.com/Promptonauts/Pipe/pkg/models"
+	"github.com/Promptonauts/pipe/pkg/models"
+)
+
+type Store interface {
+	Put(resource *models.GenericResource) error
+	Get(kind models.ResourceKind, namespace, name string) (*models.GenericResource, error)
+	List(kind models.ResourceKind, namespace string) ([]*models.GenericResource, error)
+	Delete(kind models.ResourceKind, namespace, name string, status models.ResourceStatus) error
+
+	CreateExecution(exec *models.ExecutionRecord) error
+	GetExecution(id string) (*models.ExecutionRecord, error)
+	UpdateExecution(Exec *models.ExecutionRecord) error
+	ListExecutions(namespace string, limit int) ([]*models.ExecutionRecord, error)
+	GetExecutionLogs(id string) ([]models.ExecutionLog, error)
+	AppendExecutionLog(id string, log models.ExecutionLog) error
+	SaveCheckpoint(executionID string, data []byte) error
+	LoadCheckpoint(executionID string) ([]byte, error)
+
+	Watch(kind models.ResourceKind) <-chan ResourceEvent
+
+	Migrate() error
+	Close() error
+}
+
+type EventType string
+
+const (
+	EventCreated EventType = "CREATED"
+	EventUpdated EventType = "UPDATED"
+	EventDeleted EventType = "DELETED"
+)
+
+type ResourceEvent struct {
+	Type     EventType
+	Resource *models.GenericResource
+}
